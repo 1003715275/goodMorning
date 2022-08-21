da6b0ce1177c3f7cd41f0960200940a9b9cc7e3c
1003715275 <48754484+1003715275@users.noreply.github.com>
2022-08-21 23:43:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import random
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
+born_date = os.environ['BORN_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 
@@ -26,7 +27,7 @@ def get_weather():
 
 def get_count():
   delta_day = today - datetime.strptime(start_date, "%Y-%m-%d")
-  survive_day = today - datetime.strptime("1998-05-12", "%Y-%m-%d")
+  survive_day = today - datetime.strptime(born_date, "%Y-%m-%d")
   return delta_day.days, survive_day.days
 
 def get_birthday():
